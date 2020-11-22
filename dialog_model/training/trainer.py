d9df3c379f78da5a2804f51dbdf929c1992ec4f7
akarnachev <a.karnachev@just-ai.com>
2020-11-22 23:52:17 +0300

    In dev.

--- dialog_model/training/trainer.py
+++ dialog_model/training/trainer.py
@@ -85,9 +85,9 @@ class Trainer:
                 scaler.step(optimizer)
                 scaler.update()
 
-                # if rank == 0:
-                #     log_postfix.update({'loss/Train': dist.all_reduce(model_output.loss) / self._world_size})
-                #     train_dl.set_postfix(log_postfix)
+                if rank == 0:
+                    log_postfix.update({'loss/Train': dist.all_reduce(model_output.loss) / self._world_size})
+                    train_dl.set_postfix(log_postfix)
 
         dist.destroy_process_group()
 
