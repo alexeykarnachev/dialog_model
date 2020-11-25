6ff2c7dfadc1f751accb923dae29616d0300ad7d
akarnachev <a.karnachev@just-ai.com>
2020-11-25 17:15:18 +0300

    In dev.

--- dialog_model/tokenization/dialogs_tokenizer.py
+++ dialog_model/tokenization/dialogs_tokenizer.py
@@ -36,7 +36,7 @@ class DialogsTokenizer:
         self._dtype = np.uint16 if self._tokenizer.vocab_size < 65500 else np.int32
 
         self._end_of_prefix_token_id = self._tokenizer.convert_tokens_to_ids(END_OF_PREFIX)
-        self._start_of_utterance_token_id = self._tokenizer.convert_tokens_to_string(START_OF_UTTERANCE)
+        self._start_of_utterance_token_id = self._tokenizer.convert_tokens_to_ids(START_OF_UTTERANCE)
         self._pad_token_id = 0
 
     @property
