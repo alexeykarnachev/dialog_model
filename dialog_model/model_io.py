0c9ba6cd76915a39fe62f0fc7489f70bedaa0b49
akarnachev <a.karnachev@just-ai.com>
2020-11-29 20:14:23 +0300

    In dev.

--- dialog_model/model_io.py
+++ dialog_model/model_io.py
@@ -1,7 +1,7 @@
 import re
 
 from transformers import GPT2LMHeadModel
-
+from transformers.modeling_gpt2 import GPT2LMHeadModel
 
 def get_pretrained_gpt2_with_lm_head(name_or_path, vocab_size=None, freeze_n_layers=None):
     model = GPT2LMHeadModel.from_pretrained(name_or_path, output_hidden_states=True)
