5b03a36c388697795e8a1d219ddee49900c7eee6
akarnachev <a.karnachev@just-ai.com>
2020-11-29 20:55:10 +0300

    In dev.

--- dialog_model/utils.py
+++ dialog_model/utils.py
@@ -17,7 +17,10 @@ def iterate_on_parts_by_condition(iterable, condition):
 def get_file_md5_checksum(file_path):
     with open(file_path, "rb") as f:
         file_hash = hashlib.md5()
-        while chunk := f.read(8192):
+        while True:
+            chunk = f.read(8192)
+            if not chunk:
+                break
             file_hash.update(chunk)
 
     return file_hash.hexdigest()
