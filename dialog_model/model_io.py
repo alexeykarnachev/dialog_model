26a8f3d91e45d6c1690d7c2a93831e8cd970f80a
akarnachev <a.karnachev@just-ai.com>
2020-11-29 20:15:46 +0300

    In dev.

--- dialog_model/model_io.py
+++ dialog_model/model_io.py
@@ -11,19 +11,23 @@ def get_pretrained_gpt2_with_lm_head(name_or_path, vocab_size=None, freeze_n_lay
         _resize_embeddings(model=model, vocab_size=vocab_size)
 
     if freeze_n_layers is not None:
-        freeze_layers_prefixes = {f'transformer.h.{i}.' for i in range(freeze_n_layers)}
-        frozen_layer_prefixes = set()
-        for name, param in model.named_parameters():
-            match = re.search(r'transformer\.h\.\d+\.', name)
-            if match:
-                layer_prefix = match.group(0)
-                if layer_prefix in freeze_layers_prefixes:
-                    param.requires_grad = False
-                    frozen_layer_prefixes.add(layer_prefix)
-
-        assert len(frozen_layer_prefixes) == len(freeze_layers_prefixes)
+        _freeze_layers(model=model, freeze_n_layers=freeze_n_layers)
 
     return model
+
+
+def _freeze_layers(model, freeze_n_layers: int):
+    freeze_layers_prefixes = {f'transformer.h.{i}.' for i in range(freeze_n_layers)}
+    frozen_layer_prefixes = set()
+    for name, param in model.named_parameters():
+        match = re.search(r'transformer\.h\.\d+\.', name)
+        if match:
+            layer_prefix = match.group(0)
+            if layer_prefix in freeze_layers_prefixes:
+                param.requires_grad = False
+                frozen_layer_prefixes.add(layer_prefix)
+
+    assert len(frozen_layer_prefixes) == len(freeze_layers_prefixes)
 
 
 def _resize_embeddings(model, vocab_size: int):
